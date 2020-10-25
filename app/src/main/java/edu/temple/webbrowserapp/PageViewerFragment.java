c93088ee1291aa82476ba1de166922ad9247c589
anthonylittlejohn <tuh40168@temple.edu>
2020-10-25 12:05:48 -0400

    PageViewerFragment displays google.com when loaded for the first time.

--- app/src/main/java/edu/temple/webbrowserapp/PageViewerFragment.java
+++ app/src/main/java/edu/temple/webbrowserapp/PageViewerFragment.java
@@ -9,11 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
 public class PageViewerFragment extends Fragment {
 
     View l;
+    WebView webView;
+
     public PageViewerFragment() {
         // Required empty public constructor
     }
@@ -29,8 +32,8 @@ public class PageViewerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         l =  inflater.inflate(R.layout.fragment_page_viewer, container, false);
-
-        l.setBackgroundColor(Color.BLUE);
+        webView = l.findViewById(R.id.myWebView);
+        webView.loadUrl("https://www.google.com");
 
         return l;
     }
