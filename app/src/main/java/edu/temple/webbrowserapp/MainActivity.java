934ea08f5967f1ef0cf3365d23ae40f8157c51d6
anthonylittlejohn <tuh40168@temple.edu>
2020-10-25 11:42:08 -0400

    Image buttons and search bar fit screen inside PageControlFragment, Web View added to PageViewerFragment

--- app/src/main/java/edu/temple/webbrowserapp/MainActivity.java
+++ app/src/main/java/edu/temple/webbrowserapp/MainActivity.java
@@ -1,14 +1,39 @@
 package edu.temple.webbrowserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    PageControlFragment pageControlFragment;
+    PageViewerFragment pageViewerFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pageControlFragment = new PageControlFragment();
+        pageViewerFragment = new PageViewerFragment();
+
+        FragmentManager fragmentManager =  getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(!(getSupportFragmentManager().findFragmentById(R.id.page_control) instanceof PageControlFragment))
+        {
+            fragmentTransaction.add(R.id.page_control,pageControlFragment);
+        }
+        if(!(getSupportFragmentManager().findFragmentById(R.id.page_viewer) instanceof PageViewerFragment))
+        {
+            fragmentTransaction.add(R.id.page_viewer,pageViewerFragment);
+        }
+
+
+        fragmentTransaction.commit();
     }
 }
