2e23d5bddb614f3aa59968e60cddc99d8becdcb4
anthonylittlejohn <tuh40168@temple.edu>
2020-11-08 10:07:56 -0500

    three fragments added. BrowserControlFragment, PageListFragment, and PagerFragment

---
+++ app/src/main/java/edu/temple/webbrowserapp/BrowserControlFragment.java
@@ -0,0 +1,24 @@
+package edu.temple.webbrowserapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BrowserControlFragment extends Fragment {
+
+
+    public BrowserControlFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_browser_control, container, false);
+    }
+}
---
+++ app/src/main/java/edu/temple/webbrowserapp/PagerFragment.java
@@ -0,0 +1,25 @@
+package edu.temple.webbrowserapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagerFragment extends Fragment {
+
+    public PagerFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pager_frament, container, false);
+    }
+}
